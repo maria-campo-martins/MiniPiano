065e651428c467bd62a0b4d295bdaf48aa1e8f8a
Maria Campo Martins <mariacampomartins@gmail.com>
2023-09-12 21:26:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+document.addEventListener("keydown", function(event) {
+   var c =  event.key; 
+   switch (c) {
+        case 'a': 
+            var a = new Audio("./keys/key01.mp3"); 
+            a.play(); 
+            break;
+        case 'q': 
+            var q = new Audio("./keys/key02.mp3"); 
+            q.play(); 
+            break; 
+        case 's': 
+            var s = new Audio("./keys/key03.mp3"); 
+            s.play(); 
+            break;
+        case 'w': 
+            var w = new Audio("./keys/key04.mp3"); 
+            w.play(); 
+            break; 
+        case 'd': 
+            var d = new Audio("./keys/key05.mp3"); 
+            d.play(); 
+            break; 
+        case 'e': 
+            var e = new Audio("./keys/key06.mp3"); 
+            e.play(); 
+            break; 
+        case 'f': 
+            var f = new Audio("./keys/key07.mp3"); 
+            f.play(); 
+            break;
+        case 'g': 
+            var g = new Audio("./keys/key08.mp3"); 
+            g.play(); 
+            break;
+        case 'r': 
+            var r = new Audio("./keys/key09.mp3"); 
+            r.play(); 
+            break;
+        case 'h': 
+            var h = new Audio("./keys/key10.mp3"); 
+            h.play(); 
+            break;
+        case 't': 
+            var t = new Audio("./keys/key11.mp3"); 
+            t.play(); 
+            break;
+        case 'j': 
+            var j = new Audio("./keys/key12.mp3"); 
+            j.play(); 
+            break;
+        case 'k': 
+            var k = new Audio("./keys/key13.mp3"); 
+            k.play(); 
+            break;
+        case 'y': 
+            var y = new Audio("./keys/key14.mp3"); 
+            y.play(); 
+            break;
+        case 'l': 
+            var l = new Audio("./keys/key15.mp3"); 
+            l.play(); 
+            break;
+        case 'u': 
+            var u = new Audio("./keys/key16.mp3"); 
+            u.play(); 
+            break; 
+        case ';': 
+            var semicolon = new Audio("./keys/key17.mp3"); 
+            semicolon.play(); 
+            break;
+        case 'i': 
+            var i = new Audio("./keys/key18.mp3"); 
+            i.play(); 
+            break; 
+        case "'": 
+            var singlequote = new Audio("./keys/key19.mp3"); 
+            singlequote.play(); 
+            break;
+        case '[': 
+            var closingbracket = new Audio("./keys/key20.mp3"); 
+            closingbracket.play(); 
+            break; 
+        case 'o': 
+            var o = new Audio("./keys/key21.mp3"); 
+            o.play(); 
+            break;
+        case ']': 
+            var o2 = new Audio("./keys/key22.mp3"); 
+            o2.play(); 
+            break;
+        case 'p': 
+            var p = new Audio("./keys/key23.mp3"); 
+            p.play(); 
+            break;
+        case "\\": 
+            var last = new Audio("./keys/key24.mp3"); 
+            last.play(); 
+            break;              
+   }
+})
